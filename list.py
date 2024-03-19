7acad72e3f2ce16b184c998df77ce2c3cf3ac11a
little-techwiz <151989846+little-techwiz@users.noreply.github.com>
2024-03-19 20:28:32 +0300

    list.py assignment 2 Python PLP

--- list.py
+++ list.py
@@ -2,7 +2,10 @@
 my_list = []
 
 # Append the following elements to my_list: 10, 20, 30, 40
-my_list.extend([10, 20, 30, 40])
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
 
 # Insert the value 15 at the second position in the list
 my_list.insert(1, 15)
